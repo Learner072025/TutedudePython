65a2c6d40a8a2e4440e0494b8cc92737862059a3
Learner072025 <whitehatjrrashmi@email.com>
2025-07-27 02:53:18 +0530

    Added Assg1

---
+++ Assg1.py
@@ -0,0 +1,23 @@
+'''
+#Task1
+a=input('Enter the first number: ')
+b=input('Enter the second number: ')
+#Convert string to int type
+a= int(a)
+b= int(b)
+
+#Addition
+print('Addition: ', a+b)
+#Subtraction
+print('Subtraction: ', a-b)
+#Multiplication
+print('Multiplication: ', a*b)
+#Division
+print('Division: ', a/b)
+'''
+print('=========================================================================================')
+#Task2
+fname= input('Enter your First name: ')
+lname= input('Enter your Last name: ')
+fullname= fname +' '+lname
+print('Hello, '+fullname+'! Welcome to Python program.' )
